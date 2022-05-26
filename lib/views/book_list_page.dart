71ad52d21973f82a91c96db2fa0bfffd0a91b3da
rayhan <raihan.andikaenginer31@gmail.com>
2022-05-26 22:19:18 +0700

    convert listview to gridview

--- lib/views/book_list_page.dart
+++ lib/views/book_list_page.dart
@@ -41,43 +41,77 @@ class BookListPage extends StatelessWidget {
               builder: (context, controller, child) => Container(
                 child: bookController!.bookListResponse == null
                     ? child
-                    : ListView.builder(
+                    : GridView.builder(
+                        padding: const EdgeInsets.all(10),
                         itemCount:
                             bookController!.bookListResponse!.books!.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                // childAspectRatio: 3 / 2,
+                                crossAxisSpacing: 10,
+                                mainAxisSpacing: 10),
                         itemBuilder: (context, index) {
                           final currentBook =
                               bookController!.bookListResponse!.books![index];
                           return GestureDetector(
                             onTap: () {
                               Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (contex) => DetailBookPage(
+                                  builder: (context) => DetailBookPage(
                                         isbn: currentBook.isbn13!,
                                       )));
                             },
-                            child: Row(
-                              children: [
-                                Image.network(
-                                  currentBook.image!,
-                                  height: 100,
-                                ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 12),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(currentBook.title!),
-                                        Text(currentBook.subtitle!),
-                                        Align(
-                                            alignment: Alignment.topLeft,
-                                            child: Text(currentBook.price!))
-                                      ],
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.2),
+                                    spreadRadius: 0,
+                                    blurRadius: 1.5,
+                                    offset: Offset(
+                                        0, 0), // changes position of shadow
+                                  )
+                                ],
+                              ),
+                              child: Column(
+                                children: [
+                                  Image.network(
+                                    currentBook.image!,
+                                    height: 100,
+                                  ),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 12),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            currentBook.title!,
+                                            style:
+                                                const TextStyle(fontSize: 12),
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                            textAlign: TextAlign.center,
+                                          ),
+                                          // Text(currentBook.subtitle!),
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                )
-                              ],
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Align(
+                                          alignment: Alignment.topLeft,
+                                          child: Text(currentBook.price!)),
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
                           );
                         }),
